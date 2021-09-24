f5c2dc6706c43ec09829bbe51fabee6c8ba3a740
Maeve O'Shea <MO212094@reigate.ac.uk>
2021-09-24 10:59:07 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number1;
+        number1 = input.nextInt() +1;
+        for (int i = 1; i < number1; i ++ ){
+            System.out.print(i);
+            System.out.print(i);
+            System.out.print(i);
+            System.out.print(i);
+        }
+
+
+
+
+
+    }
+}
